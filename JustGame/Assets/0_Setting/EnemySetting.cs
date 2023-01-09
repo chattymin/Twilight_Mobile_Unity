16dcf206e6ac37b266340c98e2e1a78fa552c3af
Dongmin <pdm001125@naver.com>
2023-01-10 01:41:12 +0900

    [FEAT] Connection
    Win&Lose panel connect

--- JustGame/Assets/0_Setting/EnemySetting.cs
+++ JustGame/Assets/0_Setting/EnemySetting.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySetting : MonoBehaviour {
-    public const int MaxHP = 50;
-    public static int HP = 50;
+    public const int MaxHP = 3;
+    public static int HP = 3;
     public static int attackLV = 5; //attackMin, attackMax; (min: LV / max: LV * 2)
     public static int defenseLV = 5; //deffenceMin, deffenceMax;
     public static int recoveryLV = 5; //recoveryMin, recoveryMax;
