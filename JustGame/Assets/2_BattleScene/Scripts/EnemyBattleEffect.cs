950f718a3d1e8a62f10b048e2384d786956bdb94
Dongmin <pdm001125@naver.com>
2023-01-09 21:43:34 +0900

    [FEAT] Effect Develope
    공격,방어 이펙트 추가

--- JustGame/Assets/2_BattleScene/Scripts/EnemyBattleEffect.cs
+++ JustGame/Assets/2_BattleScene/Scripts/EnemyBattleEffect.cs
@@ -9,12 +9,22 @@ public class EnemyBattleEffect : MonoBehaviour
     public GameObject DefenseEffect;
     public GameObject RecoveryEffect;
 
+    public GameObject EnemyStay;
+    public GameObject EnemyAttack;
+    public GameObject EnemyDefense;
+    public GameObject EnemyRecovery;
+
     // Start is called before the first frame update
     void Start()
     {
         AttackEffect.SetActive(false);
         DefenseEffect.SetActive(false);
         RecoveryEffect.SetActive(false);
+
+        EnemyStay.SetActive(true);
+        EnemyAttack.SetActive(false);
+        EnemyDefense.SetActive(false);
+        EnemyRecovery.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,16 +41,31 @@ public class EnemyBattleEffect : MonoBehaviour
                 AttackEffect.SetActive(true);
                 DefenseEffect.SetActive(false);
                 RecoveryEffect.SetActive(false);
+
+                EnemyStay.SetActive(false);
+                EnemyAttack.SetActive(true);
+                EnemyDefense.SetActive(false);
+                EnemyRecovery.SetActive(false);
                 break;
             case 2:
                 AttackEffect.SetActive(false);
                 DefenseEffect.SetActive(true);
                 RecoveryEffect.SetActive(false);
+
+                EnemyStay.SetActive(false);
+                EnemyAttack.SetActive(false);
+                EnemyDefense.SetActive(true);
+                EnemyRecovery.SetActive(false);
                 break;
             case 3:
                 AttackEffect.SetActive(false);
                 DefenseEffect.SetActive(false);
                 RecoveryEffect.SetActive(true);
+
+                EnemyStay.SetActive(false);
+                EnemyAttack.SetActive(false);
+                EnemyDefense.SetActive(false);
+                EnemyRecovery.SetActive(true);
                 break;
         }
     }
@@ -50,6 +75,11 @@ public class EnemyBattleEffect : MonoBehaviour
         AttackEffect.SetActive(false);
         DefenseEffect.SetActive(false);
         RecoveryEffect.SetActive(false);
+
+        EnemyStay.SetActive(true);
+        EnemyAttack.SetActive(false);
+        EnemyDefense.SetActive(false);
+        EnemyRecovery.SetActive(false);
     }
 
 }
